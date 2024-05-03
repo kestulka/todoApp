733f709b0f798a7a0e8602af44aa05e8f5974175
kestulka <kgirdiunas0@gmail.com>
2024-05-03 13:56:18 +0300

    pre dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./Components/Form";
 import { FormData, Task } from "./interfaces/FormInterfaces";
 import TaskTable from "./Components/TaskTable";
